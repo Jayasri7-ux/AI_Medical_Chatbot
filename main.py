0ad0745cc7c3e14407667e45afd5e0decde2b571
Jaya Sri <jayasri@gmail.com>
2026-04-30 18:44:16 +0530

    feat: prepare for hosting and remove .env from tracking

--- main.py
+++ main.py
@@ -36,4 +36,6 @@ async def read_root(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=9000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 9000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
